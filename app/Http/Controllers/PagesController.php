f3e6889f8c55870479c49670158c462b8935e288
rodofilms <rodartejr.rodolfo@hotmail.com>
2020-08-05 00:57:20 -0500

    Conexion a la base de datos, se tomaron datos de una DB y se mostraron usando Eloquent de Laravel

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App;
 
 class PagesController extends Controller
 {
     //
     public function index(){
-        return view('welcome');
+        $notas = App\Nota::all();
+        return view('welcome',compact('notas'));
     }
     public function fotos(){
         return view('bienvenido');
